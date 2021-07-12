7eefa97e12d7daa5e10dfab0023cc4983813bb07
robin <106778@student.drenthecollege.nl>
2021-07-12 14:59:55 +0200

    style.css eerst weggehaald. taxonomy aangemaakt haalt de afbeeldingen uit de posts hier moeten teksten nog worden toegevoegd

--- wp-content/plugins/Narrowcasting/narrowcasting.php
+++ wp-content/plugins/Narrowcasting/narrowcasting.php
@@ -7,6 +7,45 @@ Version: 1.0
 Author: Robin Brokmann
 */
 
+//hook into the init action and call create_topics_nonhierarchical_taxonomy when it fires
+
+add_action( 'init', 'create_topics_nonhierarchical_taxonomy', 0 );
+
+function create_topics_nonhierarchical_taxonomy() {
+
+// Labels part for the GUI
+
+    $labels = array(
+        'name' => _x( 'schermen', 'taxonomy general name' ),
+        'singular_name' => _x( 'schermen', 'taxonomy singular name' ),
+        'search_items' =>  __( 'Scermen Zoeken' ),
+        'all_items' => __( 'Alle Schermen' ),
+        'parent_item' => null,
+        'parent_item_colon' => null,
+        'edit_item' => __( 'Scherm Bewerken' ),
+        'update_item' => __( 'Update Scherm' ),
+        'add_new_item' => __( 'Nieuw Scherm Toevoegen' ),
+        'new_item_name' => __( 'Nieuw Scherm Naam' ),
+        'separate_items_with_commas' => __( 'Scheid schermen met comma`s' ),
+        'add_or_remove_items' => __( 'Schermen toevoegen en verwijderen' ),
+        'choose_from_most_used' => __( 'Kies uit de meest gebruikte schermen' ),
+        'menu_name' => __( 'Schermen' ),
+    );
+
+// Now register the non-hierarchical taxonomy like tag
+
+    register_taxonomy('scherm','schermen',array(
+        'hierarchical' => false,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_in_rest' => true,
+        'show_admin_column' => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'scherm' ),
+    ));
+}
+
 
 function custom_post_type()
 {
@@ -16,22 +55,22 @@ function custom_post_type()
         'singular_name' => _x('Narrowcasting', 'Post Type Singular Name', 'text_domain'),
         'menu_name' => __('Narrowcasting', 'text_domain'),
         'parent_item_colon' => __('Parent Item:', 'text_domain'),
-        'all_items' => __('Alle schermen', 'text_domain'),
+        'all_items' => __('Alle slides', 'text_domain'),
         'view_item' => __('Bekijken', 'text_domain'),
-        'add_new_item' => __('Nieuw Scherm Toevoegen', 'text_domain'),
-        'add_new' => __('Nieuw Scherm', 'text_domain'),
+        'add_new_item' => __('Nieuwe Slide Toevoegen', 'text_domain'),
+        'add_new' => __('Nieuwe slide', 'text_domain'),
         'edit_item' => __('Bewerken', 'text_domain'),
         'update_item' => __('Update Item', 'text_domain'),
         'search_items' => __('Zoeken', 'text_domain'),
-        'not_found' => __('Geen schermen gevonden', 'text_domain'),
+        'not_found' => __('Geen Slides gevonden', 'text_domain'),
         'not_found_in_trash' => __('Niets gevonden in de prullenbak', 'text_domain'),
     );
     $args = array(
         'label' => __('Narrowcasting', 'text_domain'),
         'description' => __('Post Type voor Narrowcasting', 'text_domain'),
         'labels' => $labels,
-        'supports' => array('title', 'editor', 'author', 'thumbnail', 'comments', 'custom-fields',),
-        'taxonomies' => array(''),
+        'supports' => array('title', 'editor', 'author', 'category'),
+        'taxonomies' => array('scherm'),
         'hierarchical' => true,
         'public' => true,
         'show_ui' => true,
@@ -65,10 +104,14 @@ add_filter( 'template_include', 'my_plugin_templates' );
 function my_plugin_templates( $template ) {
     $post_types = array( 'narrowcasting' );
 
-    if ( is_singular( $post_types ) && file_exists( plugin_dir_path(__FILE__) . 'templates/single_narrowcasting.php' ) ){
-        $template = plugin_dir_path(__FILE__) . 'templates/single_narrowcasting.php';
+    if ( is_singular( $post_types ) && file_exists( plugin_dir_path(__FILE__) . 'templates/single-narrowcasting.php' ) ){
+        $template = plugin_dir_path(__FILE__) . 'templates/single-narrowcasting.php';
+    }
+    if( is_tax('scherm') && file_exists( plugin_dir_path(__FILE__) . 'templates/taxonomy-scherm.php' ) ){
+        $template = plugin_dir_path(__FILE__ ).'templates/taxonomy-scherm.php';
     }
 
     return $template;
 }
+
 
