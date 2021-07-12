d12cdac3a57ec3edf92d249ff5bf5b29f713798a
robin <106778@student.drenthecollege.nl>
2021-07-12 21:07:58 +0200

    - de slide goed werkend gemaakt op het scherm en wat aan de opmaak gedaan - tekst eerst op een vaste plek gezet mogelijk instelbaar vanuit Wordpress in een volgende versie.

--- wp-content/plugins/Narrowcasting/templates/taxonomy-scherm.php
+++ wp-content/plugins/Narrowcasting/templates/taxonomy-scherm.php
@@ -2,49 +2,72 @@
 <html <?php language_attributes(); ?>>
 <head>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <?php wp_head(); ?>
 </head>
 <body>
 <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="10000" data-transitionduration="500">
     <div class='carousel-inner' style="border-radius: 8px;">
-<?php
-$i=0;
-//haalt de data uit de post eerst nog afbeeldingen
-while ( have_posts() ) : the_post();
-    $content = get_the_content();
-    $dom = new DOMDocument;
-    if ($dom->loadHTML($content)) {
-        $imgs = $dom->getElementsByTagName('img');
-        if ($imgs->length > 0) {
-            foreach ($imgs as $img) {
-                $img->getAttribute('src');
+        <?php
+        $i = 0;
+        //haalt de data uit de post eerst nog afbeeldingen
+        while (have_posts()) : the_post();
+            $content = get_the_content();
+            $text = get_the_content();
+            $text = preg_replace("/<img[^>]+\>/i", " ", $text);
+            $text = preg_replace("/<iframe[^>]+\>/i", " ", $text);
+            $text = apply_filters('the_content', $text);
+            $text = str_replace(']]>', ']]>', $text);
+
+            $dom = new DOMDocument;
+            if ($dom->loadHTML($content)) {
+                $imgs = $dom->getElementsByTagName('img');
             }
-        }
-    }
-    ?>
+            if ($imgs->length > 0) {
+                foreach ($imgs as $img) {
+                    $img->getAttribute('src');
+                }
+            }
+            ?>
 
-    <!--zet de afbeeldingen in een slide. -->
+            <!--zet de afbeeldingen en tekst in een slide. -->
 
 
-            <?php  foreach ( $imgs as $img): ?>
-                <?php if ($i==0) {$set_ = 'active'; } else {$set_ = ''; } ?>
-                <div class='carousel-item <?php echo $set_; ?>'>
-                    <img src='<?php echo $img->getAttribute('src'); ?>' class='d-block h-100 w-100'>
+            <?php foreach ($imgs as $img):
+                if ($i == 0) {
+                    $set_ = 'active';
+                } else {
+                    $set_ = '';
+                } ?>
+                <div class='carousel-item <?php echo $set_; ?>' style="position: relative;">
+                    <img src='<?php echo $img->getAttribute('src'); ?>' class='d-block w-100' style="position: fixed;">
+                    <div class="text-box"
+                         style=" background: rgba(26,26,26,0.23); width: 700px; z-index:100;  border-radius: 10px; position: fixed;  top: 200px; right: 100px; ">
+                        <div class="title" style="color: #535353; font-size: 50px; text-align: center ">
+                            <?php echo get_the_title() ?>
+                        </div>
+                        <div class="text" style="color: #535353; text-align: center;">
+                            <?php echo $text; ?>
+                        </div>
+                    </div>
                 </div>
                 <?php $i++; endforeach ?>
 
-<?php
-endwhile;
-?>
+        <?php
+        endwhile;
+        ?>
 
     </div>
-</div>
 </body>
 </html>
 
